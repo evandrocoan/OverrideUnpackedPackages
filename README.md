fb11e8a1f4807da2a1f295e76ceab9ad46f53bb8
evandrocoan <evandrocoan@hotmail.com>
2017-06-19 21:01:14 -0300

    To allow you override default menu as files `.sublime-menu`, you
    will need to rename the files you put on the folder `Override
    Unpacked Packages` to `.sublime-menu.hide`. This way Sublime Text
    will not double load your menus.
    
    When the `Override Unpacked Packages` will perform the override,
    it will rename them back to their original names as the
    `.sublime-menu` mentioned just above.

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ You need to create the replacement file on the folder:
 Then run the command `Override Unpacked Packages Now` on the command palette or just restart Sublime
 Text.
 
+
+### Settings
+
+To allow you override default menu as files `.sublime-menu`, you will need to rename the files you
+put on the folder `Override Unpacked Packages` to `.sublime-menu.hide`. This way Sublime Text
+will not double load your menus.
+
+When the `Override Unpacked Packages` will perform the override, it will rename them back to their
+original names as the `.sublime-menu` mentioned just above.
+
+
--- override_unpacked_packages.py
+++ override_unpacked_packages.py
@@ -1,10 +1,11 @@
 
 import os
+import shutil
 
 import sublime
 import sublime_plugin
 
-from distutils.dir_util import copy_tree
+from threading import Thread
 
 
 PACKAGE_NAME = "Override Unpacked Packages"
@@ -31,15 +32,64 @@ class OverrideUnpackedPackagesNowCommand(sublime_plugin.TextCommand):
 
 def load_overrides():
     """
+        Creating Threads in python
+        https://stackoverflow.com/questions/2905965/creating-threads-in-python
+    """
+    thread = Thread( target=_load_overrides )
+    thread.start()
+
+    # If we join the thread, we would hang Sublime Text until the copy if finished
+    # thread.join()
+    # print( "( load_overrides ) thread finished coping files." )
+
+
+def _load_overrides():
+    """
         Copy directory contents into a directory with python
         https://stackoverflow.com/questions/15034151/copy-directory-contents-into-a-directory-with-python
     """
-    files = os.listdir( SETTINGS_FOLDER )
-
+    # files = os.listdir( SETTINGS_FOLDER )
     # for file in files:
     #     print( "file: " + str( file ) )
+    copy_overrides( SETTINGS_FOLDER, PACKAGES_PATH )
 
-    copy_tree( SETTINGS_FOLDER, PACKAGES_PATH )
 
+def copy_overrides( root_source_folder, root_destine_folder, move_files=False ):
+    """
+        Python How To Copy Or Move Folders Recursively
+        http://techs.studyhorror.com/python-copy-move-sub-folders-recursively-i-92
+
+        Python script recursively rename all files in folder and subfolders
+        https://stackoverflow.com/questions/41861238/python-script-recursively-rename-all-files-in-folder-and-subfolders
+
+        Force Overwrite in Os.Rename
+        https://stackoverflow.com/questions/8107352/force-overwrite-in-os-rename
+    """
+    # Call this if operation only one time, instead of calling the for every file.
+    if move_files:
+        def copy_file():
+            shutil.move( source_file, destine_folder )
+    else:
+        def copy_file():
+            shutil.copy( source_file, destine_folder )
+
+    for source_folder, directories, files in os.walk( root_source_folder ):
+        destine_folder = source_folder.replace( root_source_folder, root_destine_folder)
+
+        if not os.path.exists( destine_folder ):
+            os.mkdir( destine_folder )
+
+        for file in files:
+            source_file  = os.path.join( source_folder, file )
+            destine_file = os.path.join( destine_folder, file )
+
+            if os.path.exists( destine_file ):
+                os.remove( destine_file )
+
+            # print( ( "Moving" if move_files else "Coping" ), "file:", source_file, "to", destine_file )
+            copy_file()
+
+            if file.endswith(".hide"):
+                os.replace( destine_file, destine_file[0:-5] )
 
 
